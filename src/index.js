78750e75e2b3b487efc11e26151438b34d9753c6
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-03 21:55:48 -0600

    added jsdocstrings to my two class methods

--- src/index.js
+++ src/index.js
@@ -1,131 +1,196 @@
-const longMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December']
-const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-const longDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const shortDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const longMonths = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "November",
+  "December",
+];
+const shortMonths = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+const longDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const shortDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 class dateObject {
-    constructor(...args) {
-        this._date = new Date(...args)
+  constructor(...args) {
+    this._date = new Date(...args);
+  }
+  get year() {
+    return this._date.getFullYear();
+  }
+  get yr() {
+    return this._date.getFullYear() % 100;
+  }
+  get month() {
+    return longMonths[this._date.getMonth()];
+  }
+  get mon() {
+    return shortMonths[this._date.getMonth()];
+  }
+  get day() {
+    return longDays[this._date.getDay()];
+  }
+  get dy() {
+    return shortDays[this._date.getDay()];
+  }
+  get date() {
+    return this._date.getDate();
+  }
+  get padDate() {
+    let stringDate = this._date.getDate().toString();
+    return Number(stringDate) > 9 ? stringDate : "0" + stringDate;
+  }
+  get hours() {
+    return this._date.getHours();
+  }
+  get padHours() {
+    let stringDate = this._date.getHours().toString();
+    return Number(stringDate) > 9 ? stringDate : "0" + stringDate;
+  }
+  get mins() {
+    return this._date.getMinutes();
+  }
+  get padMins() {
+    let stringDate = this._date.getMinutes().toString();
+    return Number(stringDate) > 9 ? stringDate : "0" + stringDate;
+  }
+  get secs() {
+    return this._date.getSeconds();
+  }
+  get padSecs() {
+    let stringDate = this._date.getSeconds().toString();
+    return Number(stringDate) > 9 ? stringDate : "0" + stringDate;
+  }
+  get ordinal() {
+    let stringDate = this._date.getDate().toString();
+    let lastChar = stringDate.slice(-1);
+    switch (lastChar) {
+      case "1":
+        stringDate += "st";
+        break;
+      case "2":
+        stringDate += "nd";
+        break;
+      case "3":
+        stringDate += "rd";
+        break;
+      default:
+        stringDate += "th";
+        break;
     }
-    get year() {
-        return this._date.getFullYear()
+    return stringDate;
+  }
+  /**
+   * This function formats a date using a mask.
+   * @param {String} input format mask
+   * @returns {String} formatted string of dateObject
+   */
+  format = (mask) => {
+    const formatObject = {
+      Y: this.year,
+      y: this.yr,
+      M: this.month,
+      m: this.mon,
+      D: this.padDate,
+      d: this.date,
+      H: this.padHours,
+      h: this.hours,
+      I: this.padMins,
+      i: this.mins,
+      S: this.padSecs,
+      s: this.secs,
+      W: this.day,
+      w: this.dy,
+      o: this.ordinal,
+    };
+    if (!mask) {
+      return `${this.year} ${this.month} ${this.padDate}`;
     }
-    get yr() {
-        return this._date.getFullYear() % 100
+    let formattedArray = [];
+    const splitMask = mask.split("");
+    for (let i = 0; i < splitMask.length; i++) {
+      if (formatObject[splitMask[i]]) {
+        const itemToAdd = formatObject[splitMask[i]];
+        formattedArray.push(itemToAdd);
+      } else {
+        const itemToAdd = splitMask[i];
+        formattedArray.push(itemToAdd);
+      }
     }
-    get month() {
-        return longMonths[this._date.getMonth()]
+    return formattedArray.join("");
+  };
+  /**
+   * This function returns a string indicating how far away a date is from now. 
+   * @returns {String} string describing how far in the past or future a dateObject is from now. 
+   */
+  when = () => {
+    let past = false;
+    let date = this._date;
+    let now = new dateObject();
+    now = now._date;
+
+    if (now > date) {
+      past = true;
     }
-    get mon() {
-        return shortMonths[this._date.getMonth()]
+    if (now.getFullYear() != date.getFullYear()) {
+      const difference = Math.abs(now.getFullYear() - date.getFullYear());
+      return past ? difference + " years ago" : difference + " years from now";
     }
-    get day() {
-        return longDays[this._date.getDay()]
+    if (now.getMonth() != date.getMonth()) {
+      const difference = Math.abs(now.getMonth() - date.getMonth());
+      return past
+        ? difference + " months ago"
+        : difference + " months from now";
     }
-    get dy() {
-        return shortDays[this._date.getDay()]
+    if (now.getDate() != date.getDate()) {
+      const difference = Math.abs(now.getDate() - date.getDate());
+      return past ? difference + " days ago" : difference + " days from now";
     }
-    get date() {
-        return this._date.getDate()
+    if (now.getHours() != date.getHours()) {
+      const difference = Math.abs(now.getHours() - date.getHours());
+      return past ? difference + " hours ago" : difference + " hours from now";
     }
-    get padDate() {
-        let stringDate = this._date.getDate().toString()
-        return Number(stringDate) > 9 ? stringDate : "0" + stringDate
+    if (now.getMinutes() != date.getMinutes()) {
+      const difference = Math.abs(now.getMinutes() - date.getMinutes());
+      return past
+        ? difference + " minutes ago"
+        : difference + " minutes from now";
     }
-    get hours() {
-        return this._date.getHours()
+    if (now.getSeconds() != date.getSeconds()) {
+      const difference = Math.abs(now.getSeconds() - date.getSeconds());
+      return past
+        ? difference + " seconds ago"
+        : difference + " seconds from now";
     }
-    get padHours() {
-        let stringDate = this._date.getHours().toString()
-        return Number(stringDate) > 9 ? stringDate : "0" + stringDate
-    }
-    get mins() {
-        return this._date.getMinutes()
-    }
-    get padMins() {
-        let stringDate = this._date.getMinutes().toString()
-        return Number(stringDate) > 9 ? stringDate : "0" + stringDate
-    }
-    get secs() {
-        return this._date.getSeconds()
-    }
-    get padSecs() {
-        let stringDate = this._date.getSeconds().toString()
-        return Number(stringDate) > 9 ? stringDate : "0" + stringDate
-    }
-    format = (mask) => {        
-        const formatObject = {
-            'Y': this.year,
-            'y': this.yr,
-            'M': this.month,
-            'm': this.mon,
-            'D': this.padDate,
-            'd': this.date,
-            'H': this.padHours,
-            'h': this.hours,
-            'I': this.padMins,
-            'i': this.mins,
-            'S': this.padSecs,
-            's': this.secs
-        }
-        if(!mask){
-            return `${this.year} ${this.month} ${this.padDate}`
-        }
-        let formattedArray = []
-        const splitMask = mask.split('')
-        for(let i = 0; i < splitMask.length; i++){
-            if (formatObject[splitMask[i]]){
-                const itemToAdd = formatObject[splitMask[i]]
-                formattedArray.push(itemToAdd)
-            } else {
-                const itemToAdd = splitMask[i]
-                formattedArray.push(itemToAdd)
-            }
-        }
-        return formattedArray.join('')
-    }
-    when = () => {
-        let past = false
-        let date = this._date
-        let now = new dateObject()
-        now = now._date
-        let returnArray = []
-        
-        if (now > date) {
-            past = true
-        }
-        if (now.getFullYear() != date.getFullYear() ){
-            const difference = Math.abs(now.getFullYear() - date.getFullYear())
-            return past ? difference + " years ago" : difference + " years from now"
-        }
-        if (now.getMonth() != date.getMonth()) {
-            const difference = Math.abs(now.getMonth() - date.getMonth())
-            return past ? difference + " months ago" : difference + " months from now"
-        }
-        if (now.getDate() != date.getDate()) {
-            const difference = Math.abs(now.getDate() - date.getDate())
-            return past ? difference + " days ago" : difference + " days from now"
-        }
-        if (now.getHours() != date.getHours()) {
-            const difference = Math.abs(now.getHours() - date.getHours())
-            return past ? difference + " hours ago" : difference + " hours from now"
-        }
-        if (now.getMinutes() != date.getMinutes()) {
-            const difference = Math.abs(now.getMinutes() - date.getMinutes())
-            return past ? difference + " minutes ago" : difference + " minutes from now"
-        }
-        if (now.getSeconds() != date.getSeconds()) {
-            const difference = Math.abs(now.getSeconds() - date.getSeconds())
-            return past ? difference + " seconds ago" : difference + " seconds from now"
-        }
-    }
-    
+  };
 }
-
-
 
 // ms / 1000 / 60 / 60 / 24 / 365.25
 
-
-const testDate = new dateObject(2022, 1, 2, 14, 28, 0)
-console.log(testDate.when())
+const testDate = new dateObject(2022, 1, 21, 14, 28, 0);
+console.log(testDate.format());
